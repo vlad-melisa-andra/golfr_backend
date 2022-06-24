1e251b885f7e8f3b0ff9c56c0d5ad05ff1a44d6c
Melisa <eu_melisa_andra@yahoo.com>
2022-06-24 15:17:41 +0300

    Make rubocop pass

--- app/controllers/api/scores_controller.rb
+++ app/controllers/api/scores_controller.rb
@@ -5,7 +5,6 @@ module Api
     before_action :validate_score_user_id, only: :destroy
 
     def user_feed
-
       scores = Score.all.includes(:user).order(played_at: :desc, id: :desc)
       serialized_scores = scores.map(&:serialize)
 
